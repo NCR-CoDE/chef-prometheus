557dc0736c75534ddd837fe218769842cccc0705
Apostolakis, Nick <Nick.Apostolakis@ncr.com>
2018-03-27 11:31:59 +0100

    [ATECH-5787] modifying the job custom resource to convert target from array to string

--- resources/job.rb
+++ resources/job.rb
@@ -4,5 +4,5 @@ actions :delete
 attribute :name,                kind_of: String, name_attribute: true, required: true
 attribute :scrape_interval,     kind_of: String
 attribute :scrape_timeout,      kind_of: String
-attribute :target,              kind_of: [Array, String]
+attribute :target,              kind_of: String
 attribute :metrics_path,        kind_of: String, default: '/metrics'
